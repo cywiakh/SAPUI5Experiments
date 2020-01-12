c927ef1d0cf6025edfccbf80e2b99eeab05f19d5
Harry Cywiak <cywiakh2@gmail.com>
2020-01-12 14:25:51 +0200

    Initial Commit

---
+++ WebContent/App.controller.js
@@ -0,0 +1,94 @@
+sap.ui.define([
+		'jquery.sap.global',
+		'sap/ui/core/mvc/Controller',
+		'sap/ui/model/json/JSONModel'
+	], function(jQuery, Controller, JSONModel) {
+	"use strict";
+
+	var PageController = Controller.extend("Quickstart.App", {
+
+		onInit: function () {
+			var oData = {
+				"SelectedProduct": "HT-1001",
+                "SelectedIcon": "sap-icon://paper-plane",
+				"ProductCollection": [
+					{
+						"ProductId": "HT-1001",
+						"Name": "Notebook Basic 17",
+						"Icon": "sap-icon://paper-plane"
+					},
+					{
+						"ProductId": "HT-1002",
+						"Name": "Notebook Basic 18",
+						"Icon": "sap-icon://add-document"
+					},
+					{
+						"ProductId": "HT-1003",
+						"Name": "Notebook Basic 19",
+						"Icon": "sap-icon://doctor"
+					},
+					{
+						"ProductId": "HT-1007",
+						"Name": "ITelO Vault",
+						"Icon": "sap-icon://sys-find-next"
+					},
+					{
+						"ProductId": "HT-1010",
+						"Name": "Notebook Professional 15",
+						"Icon": "sap-icon://add-product"
+					},
+					{
+						"ProductId": "HT-1011",
+						"Name": "Notebook Professional 17",
+						"Icon": "sap-icon://add-product"
+					},
+					{
+						"ProductId": "HT-1020",
+						"Name": "ITelO Vault Net",
+						"Icon": "sap-icon://add-product"
+					},
+					{
+						"ProductId": "HT-1021",
+						"Name": "ITelO Vault SAT",
+						"Icon": "sap-icon://add-product"
+					},
+					{
+						"ProductId": "HT-1022",
+						"Name": "Comfort Easy",
+						"Icon": "sap-icon://add-product"
+					},
+					{
+						"ProductId": "HT-1023",
+						"Name": "Comfort Senior",
+						"Icon": "sap-icon://add-product"
+					}
+				]
+			};
+
+			// set explored app's demo model on this sample
+			var oModel = new JSONModel(oData);
+			this.getView().setModel(oModel);
+		},
+
+		onChange: function () {
+           var product_key = this.getView().byId("mydd").getSelectedKey();
+           var label = this.getView().byId("cc");
+
+           var data = this.getView().getModel().getData();
+
+           var selIndex = 0;
+           for(var i=0;i<data.ProductCollection.length;i++) {
+               if(data.ProductCollection[selIndex].ProductId == product_key) {
+                   break;
+               }
+               selIndex++;
+           }
+           var icon_ref = data.ProductCollection[selIndex].Icon;
+
+            this.getView().getModel().setProperty("/SelectedIcon", icon_ref)
+
+        }
+	});
+
+	return PageController;
+});
